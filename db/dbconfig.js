dbb96cdce2ba81b07e088dcfa9b19f9178169e61
Manki Kim <cpro95@gmail.com>
2019-10-29 22:23:23 +0900

    dbconfig dbfilename fixed to packaging

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -1,7 +1,18 @@
 const path = require('path');
+const isDev = require('electron-is-dev');
 
-// console.log(path.dirname(__dirname));
-const dbFileName = path.join(path.dirname(__dirname), "./extraResources/MyVideos107.db");
+var dbFileName;
+if (isDev && process.argv.indexOf('--noDevServer') === -1) {
+  dbFileName = path.join(path.dirname(__dirname), 'extraResources', 'MyVideos107.db');
+} else {
+  dbFileName = path.join(
+    process.resourcesPath,
+    'extraResources',
+    'MyVideos107.db'
+  );
+}
+
+// console.log(dbFileName);
 // const dbFileName = './MyVideos107.db';
 
 module.exports = dbFileName;
